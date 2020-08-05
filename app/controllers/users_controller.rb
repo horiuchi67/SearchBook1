4c0d8b4c2260c35944d2304b1e5ec39823a7baed
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-05 16:09:58 +0900

    Userのindex作成中です。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
 
   def show
-  	  @user = User.find(params[:id])
-  	  @book = Book.new
-  	  @books = Book.all
+	@user = User.find(params[:id])
+	@book = Book.new
+	@books = Book.all
+  end
+  def index
+  	@users = User.all #一覧表示するためにUserモデルのデータを全て変数に入れて取り出す。
+  	@book = Book.new #new bookの新規投稿で必要（保存処理はbookコントローラー側で実施）
   end
 
 end
