e075be10c29e9f3595446355bc2e2a8b161dc079
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-05 14:47:44 +0900

    route.rbを編集しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#top"
   get 'home/about' => 'home#about', as: 'about'
+  resources :users, only: [:show, :index, :edit, :update]
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
