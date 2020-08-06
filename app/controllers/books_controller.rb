1af614e05988a869f92d474b0efa7292cfb837d0
horiuchi67 <1234mhoriuchi@gmail.com>
2020-08-06 16:33:30 +0900

    Bookのshowページを作成中です。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,9 @@ class BooksController < ApplicationController
   	@books = Book.all
     @book = Book.new
   end
+  def show
+  	@book = Book.find(params[:id])
+    @user = @book.user
+    @book1 = Book.new
+  end
 end
